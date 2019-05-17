91c0cc5ee100635a7dc814892798a98cf1dac76d
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-05-18 03:10:56 +0900

    fix #1

--- a.py
+++ a.py
@@ -3,31 +3,15 @@ import numpy as np
 import gmpy2
 import math
 
-def modinv(a_f, m):
-    f = np.copy(a_f)
+def modinv(f, m):
     assert isinstance(f, np.ndarray)
     assert isinstance(m, int)
-    n = a_f.shape[0]
-    g = np.identity(n, dtype=int)
-    for i in range(n):
-        for j in range(i+1,n):
-            try:
-                gmpy2.invert(int(f[j,i]), m)
-                f[i], f[j] = np.copy(f[j]), np.copy(f[i])
-                g[i], g[j] = np.copy(g[j]), np.copy(g[i])
-                break
-            except:
-                pass
-        inv = int(gmpy2.invert(int(f[i,i]), m))
-        f[i] = f[i] * inv % m
-        g[i] = g[i] * inv % m
-        for j in range(n):
-            if j != i:
-                p = f[j,i]
-                f[j] = (f[j] - f[i] * p) % m
-                g[j] = (g[j] - g[i] * p) % m
-    assert np.array_equal(f, np.identity(n, dtype=int))
-    assert np.array_equal(a_f.dot(g) % m, np.identity(n, dtype=int))
+    det = int(np.linalg.det(f))
+    if det % m == 0:
+        raise ValueError
+    det_inv = int(gmpy2.invert(det, m))
+    g = det_inv * np.round(det * np.linalg.inv(f)).astype(int) % m
+    assert np.array_equal(f.dot(g) % m, np.identity(len(f), dtype=int))
     assert isinstance(g, np.ndarray)
     return g
 
